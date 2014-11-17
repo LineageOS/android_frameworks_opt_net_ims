fb703364f4a81af69af04841b732cc81fef7ebe4
Suresh Kumar Sugguna <sureshk@codeaurora.org>
2014-11-17 18:36:37 +0530

    IMS: start and stop dtmf tones
    Support of start and stop dtmf tones in Imsphone
    
    Change-Id: I04f5d6f88088e5b77d314d4db9271fd49abf5dca
    CRs-Fixed: 751797

--- src/java/com/android/ims/internal/ImsCallSession.java
+++ src/java/com/android/ims/internal/ImsCallSession.java
@@ -839,6 +839,38 @@ public class ImsCallSession {
     }
 
     /**
+     * Starts a DTMF code. According to <a href="http://tools.ietf.org/html/rfc2833">RFC 2833</a>,
+     * event 0 ~ 9 maps to decimal value 0 ~ 9, '*' to 10, '#' to 11, event 'A' ~ 'D' to 12 ~ 15,
+     * and event flash to 16. Currently, event flash is not supported.
+     *
+     * @param c the DTMF to send. '0' ~ '9', 'A' ~ 'D', '*', '#' are valid inputs.
+     */
+    public void startDtmf(char c) {
+        if (mClosed) {
+            return;
+        }
+
+        try {
+            miSession.startDtmf(c);
+        } catch (RemoteException e) {
+        }
+    }
+
+    /**
+     * Stops a DTMF code.
+     */
+    public void stopDtmf() {
+        if (mClosed) {
+            return;
+        }
+
+        try {
+            miSession.stopDtmf();
+        } catch (RemoteException e) {
+        }
+    }
+
+    /**
      * Sends an USSD message.
      *
      * @param ussdMessage USSD message to send
