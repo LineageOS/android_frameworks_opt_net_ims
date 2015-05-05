a7ced64be6f5c8e0e1c9e2a9a0a51c7eb324ca4d
Omkar Kolangade <omkark@codeaurora.org>
2015-05-04 17:55:13 -0700

    IMS RAT Reporting in Registration Callbacks
    Modifying the registrationProgressing and
    registrationConnected callbacks to have a parameter
    for IMS Radio Access Technology information.
    Through this, frameworks is aware of the IMS
    RAT - LTE or WiFi - and can use the information
    to display appropriate UI notifications.
    
    Bug: 21902225
    Change-Id: I18af9c146be06d98f9ea9cd9c947e4cffee04a42

--- src/java/com/android/ims/ImsManager.java
+++ src/java/com/android/ims/ImsManager.java
@@ -1225,7 +1225,7 @@ public class ImsManager {
             return (mServiceClass == serviceClass);
         }
 
-        @Override
+        @Deprecated
         public void registrationConnected() {
             if (DBG) {
                 log("registrationConnected ::");
@@ -1236,10 +1236,36 @@ public class ImsManager {
             }
         }
 
-        @Override
+        @Deprecated
         public void registrationProgressing() {
             if (DBG) {
                 log("registrationProgressing ::");
+            }
+
+            if (mListener != null) {
+                mListener.onImsProgressing();
+            }
+        }
+
+        @Override
+        public void registrationConnectedWithRadioTech(int imsRadioTech) {
+            // Note: imsRadioTech value maps to RIL_RADIO_TECHNOLOGY
+            //       values in ServiceState.java.
+            if (DBG) {
+                log("registrationConnectedWithRadioTech :: imsRadioTech=" + imsRadioTech);
+            }
+
+            if (mListener != null) {
+                mListener.onImsConnected();
+            }
+        }
+
+        @Override
+        public void registrationProgressingWithRadioTech(int imsRadioTech) {
+            // Note: imsRadioTech value maps to RIL_RADIO_TECHNOLOGY
+            //       values in ServiceState.java.
+            if (DBG) {
+                log("registrationProgressingWithRadioTech :: imsRadioTech=" + imsRadioTech);
             }
 
             if (mListener != null) {
