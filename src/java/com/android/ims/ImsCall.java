1c1088887347d682b7b186027d98ad3617dcfb68
Anju Mathapati <anjucm@codeaurora.org>
2015-11-17 18:59:24 +0530

    IMS: Update host call callProfile on conference success
    Update host call callProfile on conference success
    
    Change-Id: I58ffb8214aef030bdc17b19e1112d4954e323e25
    BUG=26772683

--- src/java/com/android/ims/ImsCall.java
+++ src/java/com/android/ims/ImsCall.java
@@ -1903,6 +1903,9 @@ public class ImsCall implements ICall {
 
             listener = finalHostCall.mListener;
 
+            updateCallProfile(finalPeerCall);
+            updateCallProfile(finalHostCall);
+
             // Clear all the merge related flags.
             clearMergeInfo();
 
@@ -1938,6 +1941,20 @@ public class ImsCall implements ICall {
             }
         }
         return;
+    }
+
+    private static void updateCallProfile(ImsCall call) {
+        if (call != null) {
+            call.updateCallProfile();
+        }
+    }
+
+    private void updateCallProfile() {
+        synchronized (mLockObj) {
+            if (mSession != null) {
+                mCallProfile = mSession.getCallProfile();
+            }
+        }
     }
 
     /**
