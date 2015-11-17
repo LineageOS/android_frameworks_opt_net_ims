65d14617dfd4238b6f5b2e3728f1c78c83735d11
Ravi Paluri <rpaluri@codeaurora.org>
2015-11-17 18:59:24 +0530

    IMS-VT: Update host call callProfile on conference success
    Update host call callProfile on conference success
    
    Change-Id: I0c7e07215d809a9eb0281e961fb6d7fd1024ef79
    CRs-Fixed: 940142

--- src/java/com/android/ims/ImsCall.java
+++ src/java/com/android/ims/ImsCall.java
@@ -1904,6 +1904,9 @@ public class ImsCall implements ICall {
 
             listener = finalHostCall.mListener;
 
+            updateCallProfile(finalPeerCall);
+            updateCallProfile(finalHostCall);
+
             // Clear all the merge related flags.
             clearMergeInfo();
 
@@ -1939,6 +1942,20 @@ public class ImsCall implements ICall {
             }
         }
         return;
+    }
+
+    private static void updateCallProfile(ImsCall call) {
+        if (call != null) {
+            call.updateCallProfile();
+        }
+    }
+
+    private void updateCallProfile() {
+        synchronized (mLockObj) {
+            if (mSession != null) {
+                mCallProfile = mSession.getCallProfile();
+            }
+        }
     }
 
     /**
