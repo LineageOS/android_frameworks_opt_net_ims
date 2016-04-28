426f8b2ab5c90b4e7db01913ab8ee00a316e43d9
pengfeix <pengfeix@codeaurora.org>
2016-04-28 13:39:04 +0800

    Add UT interface to query call forward setting for service class.
    Current interface doesnot take any service class argument.
    Add a new API for querying call forward status based on service
    class.
    Change-Id: I2b64ed84d203105e7045a876fbf283cb0dc723b6
    CRs-Fixed: 901774

--- src/java/com/android/ims/ImsUt.java
+++ src/java/com/android/ims/ImsUt.java
@@ -66,6 +66,8 @@ public class ImsUt implements ImsUtInterface {
     private static final String TAG = "ImsUt";
     private static final boolean DBG = true;
 
+    private static final int SERVICE_CLASS_VOICE = (1 << 0);
+
     // For synchronization of private variables
     private Object mLockObj = new Object();
     private final IImsUt miUt;
@@ -147,14 +149,24 @@ public class ImsUt implements ImsUtInterface {
      */
     @Override
     public void queryCallForward(int condition, String number, Message result) {
+        queryCallForward(condition, number, SERVICE_CLASS_VOICE, result);
+    }
+
+    /**
+     * Retrieves the configuration of the call forward for specified service class.
+     * The return value of ((AsyncResult)result.obj) is an array of {@link ImsCallForwardInfo}
+     */
+    @Override
+    public void queryCallForward(int condition, String number,
+            int serviceClass, Message result) {
         if (DBG) {
             log("queryCallForward :: Ut=" + miUt + ", condition=" + condition
-                    + ", number=" + number);
+                    + ", number=" + number + ", class=" + serviceClass);
         }
 
         synchronized(mLockObj) {
             try {
-                int id = miUt.queryCallForward(condition, number);
+                int id = miUt.queryCFForServiceClass(condition, number, serviceClass);
 
                 if (id < 0) {
                     sendFailureReport(result,
