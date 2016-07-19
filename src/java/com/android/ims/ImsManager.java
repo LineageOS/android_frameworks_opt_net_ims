69fb81ab7ae32c0887dd7c663328eb97c7e09f81
Suresh Koleti <skolet@codeaurora.org>
2016-07-19 20:42:13 +0530

    IMS: Use valid IMS phone id to read the configurations
    Use valid IMS phone id to read the config resource.
    
    Change-Id: I4662ed9e441e59f0b6d88dde85e659ad3b0a7f3a
    CRs-Fixed: 1045115

--- src/java/com/android/ims/ImsManager.java
+++ src/java/com/android/ims/ImsManager.java
@@ -166,6 +166,7 @@ public class ImsManager {
 
     private Context mContext;
     private int mPhoneId;
+    private static int mImsPhoneId;
     private IImsService mImsService = null;
     private ImsServiceDeathRecipient mDeathRecipient = new ImsServiceDeathRecipient();
     // Ut interface for the supplementary service configuration
@@ -754,7 +755,7 @@ public class ImsManager {
      * @see #getServiceId
      */
     public int open(int serviceClass, PendingIntent incomingCallPendingIntent,
-            ImsConnectionStateListener listener) throws ImsException {
+            ImsConnectionStateListener listener, int imsPhoneId) throws ImsException {
         checkAndThrowExceptionIfServiceUnavailable();
 
         if (incomingCallPendingIntent == null) {
@@ -766,9 +767,10 @@ public class ImsManager {
         }
 
         int result = 0;
+        mImsPhoneId = imsPhoneId;
 
         try {
-            result = mImsService.open(mPhoneId, serviceClass, incomingCallPendingIntent,
+            result = mImsService.open(mImsPhoneId, serviceClass, incomingCallPendingIntent,
                     createRegistrationListenerProxy(serviceClass, listener));
         } catch (RemoteException e) {
             throw new ImsException("open()", e,
@@ -1066,8 +1068,11 @@ public class ImsManager {
         CarrierConfigManager configManager = (CarrierConfigManager) context.getSystemService(
                 Context.CARRIER_CONFIG_SERVICE);
         PersistableBundle b = null;
-        if (configManager != null) {
-            b = configManager.getConfig();
+        int[] subId = SubscriptionManager.getSubId(mImsPhoneId);
+        SubscriptionManager subscriptionManager = SubscriptionManager.from(context);
+        if (configManager != null && subId != null && subscriptionManager != null &&
+                subscriptionManager.isActiveSubId(subId[0])) {
+            b = configManager.getConfigForSubId(subId[0]);
         }
         if (b != null) {
             return b.getBoolean(key);
@@ -1283,7 +1288,7 @@ public class ImsManager {
 
             if (mContext != null) {
                 Intent intent = new Intent(ACTION_IMS_SERVICE_DOWN);
-                intent.putExtra(EXTRA_PHONE_ID, mPhoneId);
+                intent.putExtra(EXTRA_PHONE_ID, mImsPhoneId);
                 mContext.sendBroadcast(new Intent(intent));
             }
         }
