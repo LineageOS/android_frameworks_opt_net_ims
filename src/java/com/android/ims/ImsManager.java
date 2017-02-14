0484b738497bf589be6be5acbe7ccd9bb3f72336
Naveen Kalla <mrnaveen@google.com>
2017-02-14 14:55:24 -0800

    Remove static from the listener.
    To support Multi-SIM and multi-IMS stack use cases, we should have
    multiple instances of ImsManager. And each instance may need a
    separate isntance of the Listener if it needs to invoke non-static
    methods or access any fields of the ImsManager.
    
    Test: Successful compilation with this change.
    Change-Id: Idf4de1232ea54fef03a9e4a7ce1d98b8df841291
    (cherry picked from commit 196fd3bd575c8e4c5c2466079500c06287ec3e02)

--- src/java/com/android/ims/ImsManager.java
+++ src/java/com/android/ims/ImsManager.java
@@ -1525,7 +1525,7 @@ public class ImsManager {
     /**
      * Adapter class for {@link IImsRegistrationListener}.
      */
-    private static class ImsRegistrationListenerProxy extends IImsRegistrationListener.Stub {
+    private class ImsRegistrationListenerProxy extends IImsRegistrationListener.Stub {
         private int mServiceClass;
         private ImsConnectionStateListener mListener;
 
